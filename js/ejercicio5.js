f2fada9bdfeee9e67c2f98c0d867ad7ff9918728
andreir15 <90853700+andreir15@users.noreply.github.com>
2022-02-02 13:27:54 +0100

    Add files via upload

--- js/ejercicio5.js
+++ js/ejercicio5.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $("#texto").on("keypress",function(obj_evento){
-      $("miDiv").text(obj_evento.which);
+      
+      $("#caracteres").text(obj_evento.which);
     })
 })
